5f8581b8d6f802c11cef09ed17b8a7a0b8e81d33
manemsai <manemmohan.217@gmail.com>
2024-05-18 17:59:50 -0400

    Structure updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Mohan Sai',
     author_email='manemmohan.217@gmail.com',
-    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2","pandas","numpy"],
     packages=list(find_packages())  # Pass the result of find_packages() as a list
 )
